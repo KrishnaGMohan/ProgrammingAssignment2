1cd160179309dff6c9c8b18f492f2ad58f34fd8a
KrishnaGMohan <krishna.gandhimohan@hotmail.ca>
2015-03-13 10:12:11 -0400

    Commit Work 007

--- cachematrix.R
+++ cachematrix.R
@@ -3,24 +3,22 @@
 
 ## Write a short comment describing this function
 
-# The function makeCacheMatrix creates an object
-# with a set of functions to store and retrieve 
-# matrices and their inverses. 
+##-----------------------------------------------------------------
 
-# x holds the original matrix
-# matinv holds the inverse of the matrix
+# The function makeCacheMatrix creates an object # with a set of 
+# functions to store and retrieve # matrices and their inverses. 
 
+# 'x' holds the original matrix
+# 'matinv' holds the inverse of the matrix
 
-# When makeCacheMatrix is called with a matrix, the 
-# 'set function is used. The matrix # is stored in x 
-# of the parent env and the inverse of the matrix 
-# 'matinv' is set to null using the <<- operator. 
-# The 'get' function simply returns the matrix.
+# When makeCacheMatrix is called with a matrix, the # 'set' 
+# function is used. The matrix # is stored in 'x' # of the parent 
+# env and the inverse of the matrix 'matinv' is set to null using 
+# the <<- operator. The 'get' function simply returns the matrix.
 
-# The 'setinv' function stores the inverse of the 
-# matrix in 'matinv' in the parent env using the <<-
-# operator . And the 'getinv' function returns 
-# the 'matinv' matrix
+# The 'setinv' function stores the inverse of the # matrix in 
+# 'matinv' in the parent env using the <<- # operator . And the 
+# 'getinv' function returns the 'matinv' matrix.
 
 
 makeCacheMatrix <- function(x = matrix()) {
@@ -40,12 +38,10 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
-## The 'cacheSolve' function takes in an object 
-## created by the 'makeCacheMatrix'. If the inverse
-## had been previously calculated and stored then return
-## the cached value. If not then calculate, cache and 
-## then return the value
-
+# The 'cacheSolve' function takes in an object  ## created by 
+# the 'makeCacheMatrix'. If the inverse ## had been previously 
+# calculated and stored then return the cached value. If not 
+# then calculate, cache and then return the value
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
